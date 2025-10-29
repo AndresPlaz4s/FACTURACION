a8e281f884bc5e7aaba9dff37b6f6c527a407183
Andres Plazas <gnarvaez2008@gmail.com>
2025-10-29 09:35:27 -0500

    pagina lista navbar

--- facturacion/views.py
+++ facturacion/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'facturacion/navbar.html')
+    return render(request, 'facturacion/partials/navbar.html')
 
 def ventas(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         return render(request, 'facturacion/ventas.html')
-    return render(request, 'facturacion/navbar.html')
+    return render(request, 'facturacion/partials/navbar.html')
 
 def inventario(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         return render(request, 'facturacion/inventario.html')
-    return render(request, 'facturacion/navbar.html')
+    return render(request, 'facturacion/partials/navbar.html')
 
 def facturas(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         return render(request, 'facturacion/facturas.html')
-    return render(request, 'facturacion/navbar.html')
+    return render(request, 'facturacion/partials/navbar.html')
 
 def usuarios(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         return render(request, 'facturacion/usuarios.html')
-    return render(request, 'facturacion/navbar.html')
+    return render(request, 'facturacion/partials/navbar.html')
